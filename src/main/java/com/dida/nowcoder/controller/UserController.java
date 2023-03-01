4f57f6adc66b5777dae12403084c8924358f3761
dida <leeo@say521.cn>
2023-03-01 13:41:24 +0800

    开发上传用户头像功能

---
+++ src/main/java/com/dida/nowcoder/controller/UserController.java
@@ -0,0 +1,126 @@
+package com.dida.nowcoder.controller;
+
+import com.dida.nowcoder.entity.User;
+import com.dida.nowcoder.service.UserService;
+import com.dida.nowcoder.utils.CommunityUtil;
+import com.dida.nowcoder.utils.HostHolder;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${community.path.upload}")
+    private String uploadPath;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+
+    /**
+     * 账号设置页面接口
+     *
+     * @return 返回账号设置页面
+     */
+    @GetMapping("/setting")
+    public String getUserSettingPage() {
+        return "/site/setting";
+    }
+
+    /**
+     * 上传头像
+     *
+     * @param headerImage 用户头像路径
+     * @param model       模型
+     * @return
+     */
+    @PostMapping("/upload")
+    public String uploadHeader(MultipartFile headerImage, Model model) {
+        if (headerImage == null) {
+            model.addAttribute("error", "你还没有选择图片!");
+            return "/site/setting";
+        }
+
+        String fileName = headerImage.getOriginalFilename();
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        if (StringUtils.isBlank(suffix)) {
+            model.addAttribute("error", "文件的格式不正确！");
+            return "/site/setting";
+        }
+
+        //生成随机文件名
+        fileName = CommunityUtil.generateUUID() + suffix;
+        //确定文件的存储路径
+        File dest = new File(uploadPath + "/" + fileName);
+        try {
+            headerImage.transferTo(dest);
+        } catch (IOException e) {
+            logger.error("上传文件失败：" + e.getMessage());
+            throw new RuntimeException("上传文件失败，服务器发生异常：" + e);
+        }
+
+        User user = hostHolder.getUser();
+        String headerUrl = domain + contextPath + "/user/header/" + fileName;
+        userService.updateHeader(user.getId(), headerUrl);
+
+        return "redirect:/index";
+    }
+
+    @GetMapping("/header/{fileName}")
+    public void getHeader(@PathVariable("fileName") String fileName, HttpServletResponse response) {
+        //服务器存放路径
+        fileName = uploadPath + "/" + fileName;
+        //文件后缀
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        //响应图片
+        response.setContentType("image/" + suffix);
+
+        try (
+                /**
+                 * Java7的语法，这样写的作用是：
+                 *     在这里面声明的变量，在编译的时候，会为try...cache加上finally
+                 *     同时，在finally中会调用变量的close方法
+                 *     前提：在这里面声明的变量有close方法
+                 */
+                FileInputStream fis = new FileInputStream(fileName);
+                OutputStream os = response.getOutputStream();
+        ) {
+            byte[] buffer = new byte[1024];
+            int b = 0;
+            while ((b = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, b);
+            }
+        } catch (IOException e) {
+            logger.error("读取头像失败：" + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
